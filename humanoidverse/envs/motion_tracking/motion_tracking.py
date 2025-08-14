f0961b9bdde36027e7d2d5aa09fdfaed54776b54
JiaJunH810 <1287971889@qq.com>
2025-08-14 17:57:29 +0800

    limit_wrist

--- humanoidverse/envs/motion_tracking/motion_tracking.py
+++ humanoidverse/envs/motion_tracking/motion_tracking.py
@@ -666,9 +666,18 @@ class LeggedRobotMotionTracking(LeggedRobotBase):
         
         ## diff compute - kinematic joint position
         self.dif_joint_angles = ref_joint_pos - self.simulator.dof_pos
+        
         ## diff compute - kinematic joint velocity
         self.dif_joint_velocities = ref_joint_vel - self.simulator.dof_vel
-
+        
+        # 如果有限制的关节电机，只取非限制部分
+        if "is_limit_dof" in self.config.robot and self.config.robot.enable:
+            self.keep_dof_act_indices = [self.simulator.dof_names.index(name) for name in self.config.robot.is_limit_dof.keep_dof_act_names]
+            self.dif_global_body_pos[:, self.keep_dof_act_indices]
+            self.dif_global_body_rot[:, self.keep_dof_act_indices]
+            self.dif_global_body_vel[:, self.keep_dof_act_indices]
+            self.dif_global_body_ang_vel[:, self.keep_dof_act_indices]
+        
         # print(self.dif_joint_angles.max(),self.dif_global_body_pos.norm(dim=-1).max())
 
         # breakpoint() ## Examine the data structure of ref state
